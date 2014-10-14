ce9aac07a812c60dfafe44e0e2cab05aaad07a1b
Howard Wilson <howard@watsonbox.net>
2014-10-14 12:17:45 +0200

    bundle gem ruby-pocketsphinx

---
+++ lib/ruby/pocketsphinx.rb
@@ -0,0 +1,7 @@
+require "ruby/pocketsphinx/version"
+
+module Ruby
+  module Pocketsphinx
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/pocketsphinx/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Pocketsphinx
+    VERSION = "0.0.1"
+  end
+end
