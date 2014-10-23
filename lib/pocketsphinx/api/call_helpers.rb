eff175a11bfb69377c98a71faef69800363cc8fc
Howard Wilson <howard@watsonbox.net>
2014-10-23 18:06:11 +0200

    Attempt to fix rubinius CI specs

--- lib/pocketsphinx/api/call_helpers.rb
+++ lib/pocketsphinx/api/call_helpers.rb
@@ -4,9 +4,9 @@ module Pocketsphinx
 
     module CallHelpers
       def api_call(method, *args)
+        calling_method = caller[0][/`.*'/][1..-2]
         ps_api.send(method, *args).tap do |result|
           if result < 0
-            calling_method = caller[2][/`.*'/][1..-2]
             raise Error, "#{self.class.to_s.split('::').last}##{calling_method} failed with error code #{result}"
           end
         end
