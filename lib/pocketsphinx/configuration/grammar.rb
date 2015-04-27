6c0446ddb10b3106ed7a29136cd4c0b1c49f68d6
Howard Wilson <howard@watsonbox.net>
2015-04-27 16:50:13 +0200

    Decoder#unset_search no longer required in grammar configuration [Fixes #16]

--- lib/pocketsphinx/configuration/grammar.rb
+++ lib/pocketsphinx/configuration/grammar.rb
@@ -12,7 +12,6 @@ module Pocketsphinx
       # Since JSGF strings are not supported in Pocketsphinx configuration (only files),
       # we use the post_init_decoder hook to configure the JSGF
       def post_init_decoder(decoder)
-        decoder.unset_search
         decoder.set_jsgf_string(grammar.raw)
         decoder.set_search
       end
